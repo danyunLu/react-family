acc70e632edb171600036c25f0f7745ee13a5bbf
danyunLu <ldy0700@163.com>
2017-12-11 16:11:19 +0800

    css image 加载

--- react-family/src/pages/Page1/Page1.js
+++ react-family/src/pages/Page1/Page1.js
@@ -1,9 +1,12 @@
 import React,{Component} from "react";
+import "pages/Page1/Page1.css";
+import image from './image/test.png';
 export default class Page1 extends Component {
     render(){
         return (
-            <div>
-                this is page1 ; 
+            <div className="page-box">
+                this is page1 ;
+                <img src={image}/>
             </div>
         )
     }
