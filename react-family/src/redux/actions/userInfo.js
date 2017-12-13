d2305d0f92e084331cf70b5469a7fa984414694b
danyunLu <ldy0700@163.com>
2017-12-13 10:15:49 +0800

    axios middleware

--- react-family/src/redux/actions/userInfo.js
+++ react-family/src/redux/actions/userInfo.js
@@ -21,20 +21,26 @@ function getUserInfoFail() {
     }
 }
 
-export function getUserInfo() {
-    return function (dispatch) {
-        dispatch(getUserInfoRequest());
-        return fetch('http://localhost:8080/api/user.json')
-            .then((response => {
-                return response.json()
-            }))
-            .then((json) => {
-                    dispatch(getUserInfoSuccess(json))
-                }
-            ).catch(
-                () => {
-                    dispatch(getUserInfoFail());
-                }
-            )
+// export function getUserInfo() {
+//     return function (dispatch) {
+//         dispatch(getUserInfoRequest());
+//         return fetch('http://localhost:8080/api/user.json')
+//             .then((response => {
+//                 return response.json()
+//             }))
+//             .then((json) => {
+//                     dispatch(getUserInfoSuccess(json))
+//                 }
+//             ).catch(
+//                 () => {
+//                     dispatch(getUserInfoFail());
+//                 }
+//             )
+//     }
+// }
+export function getUserInfo(){
+    return{
+        types:[GET_USER_INFO_REQUEST, GET_USER_INFO_SUCCESS, GET_USER_INFO_FAIL],
+        promise: client => client.get(`http://localhost:8080/api/user.json`)
     }
 }
--- react-family/src/redux/store.js
+++ react-family/src/redux/store.js
@@ -1,7 +1,7 @@
 import {createStore,applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import promiseMiddleware from './middleware/promiseMiddleware';
 import combineReducers from './reducer.js';
 
-let store = createStore(combineReducers,applyMiddleware(thunkMiddleware));
+let store = createStore(combineReducers,applyMiddleware(promiseMiddleware));
 
 export default store;
