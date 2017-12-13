1b09069bcf2aaf8679e186e3e6e047fd1b76c82f
danyunLu <ldy0700@163.com>
2017-12-13 11:12:05 +0800

    合并webpack公共配置

--- react-family/webpack.config.js
+++ react-family/webpack.config.js
@@ -1,88 +1,36 @@
-const path = require('path');
+const merge = reqiure("webpack-merge");
 const webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');//文件压缩
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-module.exports = {
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-    /*入口*/
-    entry: {
-        app: [
-            path.join(__dirname, 'src/index.js')
-        ],
-        vendor:['react','react-router-dom','redux','react-dom','react-redux'] ,//提取公共库
-    },
+const commonConfig = require('./webpack.common.config.js');
 
-    /*输出到dist文件夹，输出文件名字为bundle.js 优化点：缓存*/
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].[chunkhash].js',
-        publicPath : '/'
-    },
+const publicConfig = {
+    devtool: "cheap-module-source-map",
     module: {
         rules: [{
-            test: /\.js$/,
-            use: ['babel-loader?cacheDirectory=true'],
-            include: path.join(__dirname, 'src')
-        }, {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192
-                }
-            }]
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            }),
         }]
     },
-    devServer: {
-        //hot:true, //模块热替换 等同命令 --hot
-        port: 8080,
-        contentBase: path.join(__dirname, './dist'),
-        historyApiFallback: true,
-        host: '0.0.0.0'
-    },
-    /**
-     * 文件路径优化 
-     */
-    resolve: {
-        alias: {
-            pages: path.join(__dirname, 'src/pages'),
-            components: path.join(__dirname, 'src/components'),
-            router: path.join(__dirname, 'src/router'),
-            actions: path.join(__dirname, 'src/redux/actions'),
-            reducers: path.join(__dirname, 'src/redux/reducers'),
-        }
-    },
     plugins: [
-        //
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(__dirname, 'src/index.html')
-        }),
-        //公共库缓存
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        }),
         new UglifyJSPlugin(), //文件压缩
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
-             }
-         }),
-         new webpack.HashedModuleIdsPlugin(),
-         new webpack.optimize.CommonsChunkPlugin({
-            name: 'runtime'
+            }
         }),
-        new CleanWebpackPlugin(['dist']),//打包优化
+        new CleanWebpackPlugin(['dist/*.*']),//打包优化
         new ExtractTextPlugin({
             filename: '[name].[contenthash:5].css',
             allChunks: true
         })
 
     ],
-    devtool: "cheap-module-source-map"
 };
+
+module.export = merge(commonConfig, publicConfig)
--- react-family/webpack.dev.config.js
+++ react-family/webpack.dev.config.js
@@ -1,72 +1,38 @@
+const merge = require('webpack-merge');
 const path = require('path');
-const webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-
+const commonConfig=require('./webpack.common.config.js');
+const devConfig = {
+    devtool: "inline-source-map",//调试
     /*入口*/
     entry: {
         app: [
             'react-hot-loader/patch',
             path.join(__dirname, 'src/index.js')
-        ],
-        vendor:['react','react-router-dom','redux','react-dom','react-redux'] ,//提取公共库
-    },
+        ],    },
 
-    /*输出到dist文件夹，输出文件名字为bundle.js 优化点：缓存*/
     output: {
-        path: path.join(__dirname, './dist'),
+        /*这里本来应该是[chunkhash]的，但是由于[chunkhash]和react-hot-loader不兼容。只能dev环境妥协*/
         filename: '[name].[hash].js',
-        chunkFilename: '[name].[chunkhash].js'
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            use: ['babel-loader?cacheDirectory=true'],
-            include: path.join(__dirname, 'src')
-        }, {
             test: /\.css$/,
             use: ['style-loader', 'css-loader'],
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192
-                }
-            }]
         }]
     },
     devServer: {
         //hot:true, //模块热替换 等同命令 --hot
-        port: 8080,
         contentBase: path.join(__dirname, './dist'),
         historyApiFallback: true,
         host: '0.0.0.0'
     },
-    /**
-     * 文件路径优化 
-     */
-    resolve: {
-        alias: {
-            pages: path.join(__dirname, 'src/pages'),
-            components: path.join(__dirname, 'src/components'),
-            router: path.join(__dirname, 'src/router'),
-            actions: path.join(__dirname, 'src/redux/actions'),
-            reducers: path.join(__dirname, 'src/redux/reducers'),
-        }
-    },
-    plugins: [
-        //
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(__dirname, 'src/index.html')
-        }),
-        //公共库缓存
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        }),
-        new webpack.HotModuleReplacementPlugin(),  //模块热替换 jsx
-
-    ],
-    devtool: "inline-source-map"
 };
+module.exports = merge({
+    customizeArray(a, b, key) {
+        /*entry.app不合并，全替换*/
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
