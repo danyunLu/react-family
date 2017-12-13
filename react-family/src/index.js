cd34f8445f5884719a263c5f91ebad1197f768bd
danyunLu <ldy0700@163.com>
2017-12-13 15:42:58 +0800

    优化目录结构

--- react-family/src/index.js
+++ react-family/src/index.js
@@ -1,31 +1,34 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Hello from "components/Hello/Hello";
-import {AppContainer} from 'react-hot-loader';
-import {Provider} from "react-redux";
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 
-import getRouter from"router/router"
+import App from "components/App/App"
 
 /**
  * 初始化
  */
-renderWithHotReload(getRouter());
+renderWithHotReload(App);
 
 /**
  * 热更新
  */
-if(module.hot){
-    module.hot.accept('./router/router',()=>{
-        const getRouter=require('router/router').default;
-        renderWithHotReload(getRouter());
+if (module.hot) {
+    module.hot.accept('components/App/App', () => {
+        const NextApp = require('components/App/App').default;
+        renderWithHotReload(NextApp);
     });
 }
-function renderWithHotReload(RootElement){
+function renderWithHotReload(RootElement) {
     ReactDom.render(
         <AppContainer>
             <Provider store={store}>
-            {RootElement}
+                <Router>
+                    <RootElement />
+                </Router>
             </Provider>
         </AppContainer>,
         document.getElementById('app')
